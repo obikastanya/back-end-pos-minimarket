655a2bbca874dd83976942821338a1b606b4d6d7
obikastanya <obikastanya@gmail.com>
2020-05-15 12:54:32 +0700

    add migrations

---
+++ database/migrations/2020_05_15_051255_create_barang_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBarangTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('barang', function (Blueprint $table) {
+            $table->id();
+            $table->char('id_jenis_barang', 50);
+            $table->char('id_supplier', 50);
+            $table->decimal('harga_jual', 19,2);
+            $table->decimal('harga_beli', 19,2);
+            $table->integer('stock');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('barang');
+    }
+}
---
+++ database/migrations/2020_05_15_053549_create_pegawai_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePegawaiTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pegawai', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_pegawai', 50);
+            $table->date('tgl_lahir');
+            $table->string('jenis_kelamin', 50);
+            $table->date('hire_date');
+            $table->char('no_telp', 20);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pegawai');
+    }
+}
